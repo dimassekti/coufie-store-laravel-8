98aa3707f4de9fcaa4b368e904293b0a275d283c
Dimas Sekti Adji <dimassekti.adji@gmail.com>
2023-01-05 02:56:50 +0700

    addon env example
    addon env example

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -7,31 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'users_id',
-        'insurance_price',
-        'transaction_status',
-        'shipping_price',
-        'total_price',
-        'code'
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'users_id',
+    'insurance_price',
+    'transaction_status',
+    'shipping_price',
+    'total_price',
+    'code'
+  ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [];
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [];
 
-    public function user()
-    {
-        return $this->belongsTo(User::class,  'users_id', 'id');
-    }
+  public function user()
+  {
+    return $this->belongsTo(User::class,  'users_id', 'id');
+  }
 }
